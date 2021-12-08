b47471fe07ca4f0023701a0be293e5c76aa91b16
HoonJo <hunmin815@naver.com>
2021-12-08 23:33:43 +0900

    $date commit

--- gitpush.sh
+++ gitpush.sh
@@ -3,28 +3,29 @@ date=`date`
 github_id="hunmin815"
 github_Token="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"
 dir="/home/ubuntu/autoTrade2/"
-echo "git add . ..." > push.log 2>&1
+
+echo "git add . ..." && echo "git add . ..." > push.log 2>&1
 echo "`git add .`" >> push.log 2>&1
-echo "" >> push.log 2>&1
+echo "" && echo "" >> push.log 2>&1 && echo "==" >> push.log 2>&1
 
-echo "git status" >> push.log 2>&1
+echo "git status" && echo "git status" >> push.log 2>&1
 echo "`git status`" >> push.log 2>&1
-echo "" >> push.log 2>&1
+echo "" && echo "" >> push.log 2>&1 && echo "==" >> push.log 2>&1
 
-echo "git commit -m $date commit" >> push.log 2>&1
+echo "git commit -m $date commit" && echo "git commit -m $date commit" >> push.log 2>&1
 echo "`git commit -m '$date commit'`" >> push.log 2>&1
-echo "" >> push.log 2>&1
+echo "" && echo "" >> push.log 2>&1 && echo "==" >> push.log 2>&1
 
-echo "git push!" >> push.log 2>&1
+echo "git push!" && echo "git push!" >> push.log 2>&1
 echo "`git clone https://$github_id:$github_Token@github.com/hunmin815/autoTrade.git`" >> push.log 2>&1
 
 result=`awk '/Unpacking objects: 100%/ && /done./' $dir"push.log"`
 
 
 if [ -z "$result"];then
-	echo "!! git push ERROR! please check push.log !!" >> push.log 2>&1
+	echo "!! git push ERROR! please check push.log !!" && echo "!! git push ERROR! please check push.log !!" >> push.log 2>&1
 else
-	echo "git push Success~!" >> push.log 2>&1
+	echo "git push Success~!" && echo "git push Success~!" >> push.log 2>&1
 fi
 
 exit 0
