ec89fcba2928d12e00dd492f2b940f5f48b6b1f4
hunmin815 <hunmin815@naver.com>
2021-12-15 02:10:39 +0900

    Feat Buy & Sell & profit (market Order)

--- UpbitAutoTrade.py
+++ UpbitAutoTrade.py
@@ -9,11 +9,13 @@ import os
 access = os.environ["access"]  # Upbit API access 키
 secret = os.environ["secret"]  # Upbit API secret 키
 
+fee = 0.9995  # 거래 수수료 0.05%
+
 # 내 잔고 조회_시작
 def get_balance(ticker):
     balances = upbit.get_balances()
     for b in balances:
-        if b["currency"] == ticker:
+        if b["currency"] == ticker: # 통화
             if b["balance"] is not None:
                 return float(b["balance"])
             else:
@@ -26,7 +28,7 @@ def get_ror(ticker, k):
     df["range"] = (df["high"] - df["low"]) * k
     df["target"] = df["open"] + df["range"].shift(1)  # target = 매수가
 
-    fee = 0.9995  # 거래 수수료 0.05%
+    
 
     df["ror"] = np.where(
         df["high"] > df["target"], df["close"] / df["target"] - fee, 1
@@ -67,6 +69,7 @@ except:
 else:
     print("내 잔고 : " + str(format(int(my_Balance), ",")) + " 원")
     print("date:" + str(datetime.datetime.now()))
+    buy_coin_price = 0.0
 
     while 1:
         try:
@@ -74,7 +77,7 @@ else:
             now = datetime.datetime.now()  # 현재시각
             start_time = get_start_time(ticker)  # 거래 시작 시각
             end_time = start_time + datetime.timedelta(days=1)  # 거래 종료 시각
-
+            
             # best_k 구하기_시작
             if best_k_run == 1:
                 ror1, next_ror, best_ror, best_k = 0.0, 0.0, 0.0, 0.1
@@ -105,6 +108,45 @@ else:
                 print("best_k : %.1f  best_ror : %f" % (best_k, best_ror))
                 best_k_run = 0
             # best_k 구하기_끝
+
+            if (
+                start_time < now < end_time - datetime.timedelta(seconds=10)
+            ):  # 9:00 ~ 다음날 8:59:50
+                target_price = round(get_target_price(ticker, best_k), 0)
+                print("target_price:", target_price)  # 매수 목표가
+                current_price = round(get_current_price(ticker), 0)
+                print("current_price:", current_price)  # 현재가
+
+                if target_price == current_price:  # 매수 목표가에 현재가 도달시
+                    my_ticker_bal = get_balance(ticker.split('-')[1]) # 종목 잔고
+                    if my_ticker_bal == None or my_ticker_bal < 1.0:  # 코인 보유 여부 (없거나 한 개라도 보유)
+                        my_krw = get_balance("KRW") # 원화 잔고
+                        print("Your_KRW_Balance:", my_krw)
+                        if my_krw > 100000.0: 
+                          my_krw = 100000.0 # 10만 원치만 매수
+                        if my_krw > 5000:  # 최소 주문금액 5000원
+                            print(now, "=== Buy_"+ticker.split('-')[1]+"===")
+                            # before_Buy_my_Balance = round(my_krw,0)
+                            upbit.buy_market_order( # 시장가 매수
+                                ticker, my_krw * fee
+                            )
+                            buy_price = (current_price * my_ticker_bal) * 0.9995  # 매수 총가
+                            time.sleep(1)
+                    else:
+                        print("== NOT BUY!_You Have Already "+ticker.split('-')[1]+" ==")
+
+            else:  # 다음날 오픈전 풀매도
+                best_k_run = 1
+                my_ticker_bal = get_balance(ticker.split('-')[1])
+                current_price = round(get_current_price(ticker), 0)
+                
+                if (current_price * my_ticker_bal) > 5000: # 보유 중인 종목의 잔고가 최소 주문금액 5000원 초과 시
+                    print("My_DOGE_Balance:", my_ticker_bal)
+                    print(now, "=== Sell_"+ticker.split('-')[1]+"_All ===")
+                    upbit.sell_market_order(ticker, my_ticker_bal) # 시장가 매도
+                    sell_price = (current_price * my_ticker_bal) * 0.9995  # 매도 총가
+                    profit = round(buy_price - sell_price, 0)  # 수익
+                    print("profit:", profit)
 
         except Exception as e:
             print(e)
