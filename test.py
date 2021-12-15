5890584f18f6390b2267b00f41b3e8acc7ce3bce
hunmin815 <hunmin815@naver.com>
2021-12-16 00:08:12 +0900

    Complete End

--- test.py
+++ test.py
@@ -6,39 +6,29 @@ import datetime
 import numpy as np
 import os
 
-# access = os.environ['access']                           # Upbit API access 키
-# secret = os.environ['secret']                           # Upbit API secret 키
+access = os.environ['access']                           # Upbit API access 키
+secret = os.environ['secret']                           # Upbit API secret 키
 
 # 내 잔고 조회_시작
-# def get_balance(ticker):
-#       balances = upbit.get_balances()
-#       for b in balances:
-#           if b['currency'] == ticker:
-#               if b['balance'] is not None:
-#                   return float(b['balance'])
-#               else:
-#                   return 0
+def get_balance(ticker):
+      balances = upbit.get_balances()
+      for b in balances:
+          if b['currency'] == ticker:
+              if b['balance'] is not None:
+                  return float(b['balance'])
+              else:
+                  return 0
 # 내 잔고 조회_끝
 
 # 로그인_시작
 try:
-  # upbit = pyupbit.Upbit(access, secret)
-  # my_Balance = get_balance("KRW")             # 내 잔고
+  upbit = pyupbit.Upbit(access, secret)
+  my_Balance = get_balance("KRW")             # 내 잔고
   print("Login OK")
   print("==========Autotrade start==========")
 except:
   print("!!Login ERROR!!")
 # 로그인_끝
 else:
-  # print("내 잔고 : "+str(format(int(my_Balance),','))+" 원")
+  print("내 잔고 : "+str(format(int(my_Balance),','))+" 원")
   print("date:"+str(datetime.datetime.now()))
-
-  a,b = 1,0
-  while 1:
-    time.sleep(2)
-    if a == 1:
-      a = a + 1
-      b = 1 
-    else:
-      if a == 2:
-        print(str(b))
