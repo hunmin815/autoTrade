a3ea5e6c43c0f7dbb60c8662b30cd34c3565c715
hunmin815 <hunmin815@naver.com>
2021-12-18 20:58:37 +0900

    Create ProcessCheck.sh

---
+++ ProcessCheck.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+cd $env_autoTrade_Dir
+PID1=$(ps -ef | grep UpbitAutoTrade.py | grep -v "grep" | awk {'print $2'} | head -n1)
+
+ReStartup=0 # 재기동 여부
+
+if [ -z $PID1 ]; then
+  python3 ProcessCheck_alertBot.py dead || python ProcessCheck_alertBot.py dead
+  sleep 1
+  $(./start.sh)
+  python3 ProcessCheck_alertBot.py restart || python ProcessCheck_alertBot.py restart
+  ReStartup=1
+else
+  python3 ProcessCheck_alertBot.py ok || python ProcessCheck_alertBot.py ok
+fi
+
+sleep 3
+
+if [ $ReStartup -eq 1 ]; then
+  PID1=$(ps -ef | grep UpbitAutoTrade.py | grep -v "grep" | awk {'print $2'} | head -n1)
+
+  if [ -z $PID1 ]; then
+    python3 ProcessCheck_alertBot.py fail || python ProcessCheck_alertBot.py fail
+  else
+    python3 ProcessCheck_alertBot.py ok || python ProcessCheck_alertBot.py ok
+  fi
+fi
+
+exit 0
