5e85eb7519467c56bf2cc4fb81f0865d54ecd9aa
hunmin815 <hunmin815@naver.com>
2021-12-18 23:03:40 +0900

    Refactor Format Change black

--- ProcessCheck_alertBot.py
+++ ProcessCheck_alertBot.py
@@ -2,7 +2,6 @@ import sys
 import requests
 import datetime
 import os
-import sys
 
 
 def post_message(token, channel, text):
@@ -13,30 +12,47 @@ def post_message(token, channel, text):
     )
     print(response)
 
+
 def nowtime():
-  now = datetime.datetime.today().strftime("%y-%m-%d %H:%M:%S") # 현재 DateTime
-  return now
+    now = datetime.datetime.today().strftime("%y-%m-%d %H:%M:%S")  # 현재 DateTime
+    return now
+
 
 myToken = os.environ["Slack_Token"]  # Access Token
 myChannel = "비트코인-자동매매"  # 채널 이름 OR 채널 ID
-ProcessStatus = str(sys.argv[1]) # 프로세스 상태 코드 (매개 변수)
+ProcessStatus = str(sys.argv[1])  # 프로세스 상태 코드 (매개 변수)
 
 try:
-  if ProcessStatus == "ok":
-    post_message(myToken, myChannel, " ")
-    post_message(myToken, myChannel, "$$📈🟢 Trading Process Alive$$\nServer Time : " + str(nowtime()))
+    if ProcessStatus == "ok":
+        post_message(myToken, myChannel, " ")
+        post_message(
+            myToken,
+            myChannel,
+            "$$📈🟢 Trading Process Alive$$\nServer Time : " + str(nowtime()),
+        )
 
-  elif ProcessStatus == "dead":
-    post_message(myToken, myChannel, " ")
-    post_message(myToken, myChannel, "!!🏴‍☠️ Trading Process Dead!!\nServer Time : " + str(nowtime()))
+    elif ProcessStatus == "dead":
+        post_message(myToken, myChannel, " ")
+        post_message(
+            myToken,
+            myChannel,
+            "!!🏴‍☠️ Trading Process Dead!!\nServer Time : " + str(nowtime()),
+        )
 
-  elif ProcessStatus == "restart":
-    post_message(myToken, myChannel, " ")
-    post_message(myToken, myChannel, "🔧 Restarting Now ...\nServer Time : " + str(nowtime()))
+    elif ProcessStatus == "restart":
+        post_message(myToken, myChannel, " ")
+        post_message(
+            myToken, myChannel, "🔧 Restarting Now ...\nServer Time : " + str(nowtime())
+        )
 
-  else:
-    post_message(myToken, myChannel, " ")
-    post_message(myToken, myChannel, "❗🔴 Sorry, Restarting Fail ... Please Check Process\nServer Time : " + str(nowtime()))
+    else:
+        post_message(myToken, myChannel, " ")
+        post_message(
+            myToken,
+            myChannel,
+            "❗🔴 Sorry, Restarting Fail ... Please Check Process\nServer Time : "
+            + str(nowtime()),
+        )
 
 except Exception as e:
     print(e)
--- SystemCheck_alertBot.py
+++ SystemCheck_alertBot.py
@@ -11,16 +11,26 @@ def post_message(token, channel, text):
     )
     print(response)
 
+
 def nowtime():
-  now = datetime.datetime.today().strftime("%y-%m-%d %H:%M:%S") # 현재 DateTime
-  return now
+    now = datetime.datetime.today().strftime("%y-%m-%d %H:%M:%S")  # 현재 DateTime
+    return now
+
 
 myToken = os.environ["Slack_Token"]  # Access Token
 myChannel = "비트코인-자동매매"  # 채널 이름 OR 채널 ID
 
 try:
     post_message(myToken, myChannel, " ")
-    post_message(myToken, myChannel, "$$🔌🟢 Trading System Alive$$\nServer Time : " + str(nowtime()))
+    post_message(
+        myToken,
+        myChannel,
+        "$$🔌🟢 Trading System Alive$$\nServer Time : " + str(nowtime()),
+    )
 except:
     post_message(myToken, myChannel, " ")
-    post_message(myToken, myChannel, "!!🏴‍☠️❗🔴 Trading System Dead!!\nServer Time : " + str(nowtime()))
+    post_message(
+        myToken,
+        myChannel,
+        "!!🏴‍☠️❗🔴 Trading System Dead!!\nServer Time : " + str(nowtime()),
+    )
