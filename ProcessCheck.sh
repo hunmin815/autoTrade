6d0a0e2765eaed857de148af2af8bf50dcae0c5b
hunmin815 <hunmin815@naver.com>
2021-12-18 21:56:54 +0900

    Fix Change remove python command

--- ProcessCheck.sh
+++ ProcessCheck.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
 cd $env_autoTrade_Dir
+
 PID1=$(ps -ef | grep UpbitAutoTrade.py | grep -v "grep" | awk {'print $2'} | head -n1)
 
 ReStartup=0 # 재기동 여부
 
 if [ -z $PID1 ]; then
-  python3 ProcessCheck_alertBot.py dead || python ProcessCheck_alertBot.py dead
+  python3 ProcessCheck_alertBot.py dead
   sleep 1
-  $(nohup python3 -u UpbitAutoTrade.py &) || $(nohup python -u UpbitAutoTrade.py &)
+  nohup python3 -u UpbitAutoTrade.py 2>&1 &
   sleep 1
-  python3 ProcessCheck_alertBot.py restart || python ProcessCheck_alertBot.py restart
+  python3 ProcessCheck_alertBot.py restart
   ReStartup=1
 else
-  python3 ProcessCheck_alertBot.py ok || python ProcessCheck_alertBot.py ok
+  python3 ProcessCheck_alertBot.py ok
 fi
 
 sleep 3
@@ -22,9 +23,9 @@ if [ $ReStartup -eq 1 ]; then
   PID1=$(ps -ef | grep UpbitAutoTrade.py | grep -v "grep" | awk {'print $2'} | head -n1)
 
   if [ -z $PID1 ]; then
-    python3 ProcessCheck_alertBot.py fail || python ProcessCheck_alertBot.py fail
+    python3 ProcessCheck_alertBot.py fail
   else
-    python3 ProcessCheck_alertBot.py ok || python ProcessCheck_alertBot.py ok
+    python3 ProcessCheck_alertBot.py ok
   fi
 fi
 
--- SystemCheck.sh
+++ SystemCheck.sh
@@ -2,6 +2,6 @@
 
 cd $env_autoTrade_Dir
 
-python3 SystemCheck_alertBot.py || python SystemCheck_alertBot.py
+python3 SystemCheck_alertBot.py
 
 exit 0
