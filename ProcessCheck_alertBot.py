f549c42c840953a292276bf38cf1ceb528d8d9f0
hunmin815 <hunmin815@naver.com>
2021-12-18 19:58:03 +0900

    Create ProcessCheck_alertBot.py

---
+++ ProcessCheck_alertBot.py
@@ -0,0 +1,26 @@
+import requests
+import datetime
+import os
+
+
+def post_message(token, channel, text):
+    response = requests.post(
+        "https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer " + token},
+        data={"channel": channel, "text": text},
+    )
+    print(response)
+
+
+myToken = os.environ["Slack_Token"]  # Access Token
+myChannel = "비트코인-자동매매"  # 채널 이름 OR 채널 ID
+
+try:
+    now = datetime.datetime.now()
+    post_message(myToken, myChannel, " ")
+    post_message(myToken, myChannel, "$$Trading Process Alive$$")
+    post_message(myToken, myChannel, "Server Time : " + str(now))
+except:
+    post_message(myToken, myChannel, " ")
+    post_message(myToken, myChannel, "!!Trading Process Dead!!")
+    post_message(myToken, myChannel, "Server Time : " + str(now))
